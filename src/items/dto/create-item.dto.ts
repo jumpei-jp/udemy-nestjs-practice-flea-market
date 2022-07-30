42c40ffbd2ffb1358659576313f24bb75f5fdb61
Jumpei Yamaoka <junpeiyamaoka0516@gmail.com>
2022-07-30 14:59:49 +0900

    feat: class-validatorの追加でitemにvalidationを実装

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,5 +1,19 @@
+import { IsNotEmpty, IsString, MaxLength, IsInt, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+
+// itemに対してvalidationをする
 export class CreateItemDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(40)
   name: string;
+
+  @IsInt()
+  @Min(1)
+  @Type(() => Number)
   price: number;
+
+  @IsString()
+  @IsNotEmpty()
   description: string;
 }
