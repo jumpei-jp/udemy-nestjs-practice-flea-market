61c2926d291478c27d3bdcd222a68265c71c70ec
Jumpei Yamaoka <junpeiyamaoka0516@gmail.com>
2022-07-30 09:38:51 +0900

    feat: createでItemを配列に保存

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from './item.model';
 
 @Injectable()
 export class ItemsService {
+  private items: Item[] = [];
   findAll() {
     return 'This is ItemsService';
+  }
+
+  // 受け取ったパラメータをitemという配列に入れて返す
+  create(item: Item): Item {
+    this.items.push(item);
+    return item;
   }
 }
